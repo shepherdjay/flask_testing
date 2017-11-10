3da1a0ab1cfa1edecf9883919ce2f6c0f29603e2
Jay Shepherd <shepherdjay@users.noreply.github.com>
2017-11-10 16:49:57 -0700

    travis build stalling, print commands to find out where

--- tests/test_flask_testing.py
+++ tests/test_flask_testing.py
@@ -9,18 +9,24 @@ TEST_SERVER_URL = "http://127.0.0.1:5000"
 
 @pytest.fixture(scope="module", autouse=True)
 def server():
+    print("Starting Server")
     server = subprocess.Popen(['python', '-m', 'flask_testing'])
     time.sleep(1)
     yield server
+    print("Terminating Server")
     server.terminate()
 
 
 @pytest.fixture()
 def browser():
+    print("Starting Browser")
     browser = webdriver.Firefox()
+    print("Setting Implicit")
     browser.implicitly_wait(5)
+    print("MAX Window")
     browser.maximize_window()
     yield browser
+    print("Closing Browser")
     browser.refresh()
     browser.quit()
 
